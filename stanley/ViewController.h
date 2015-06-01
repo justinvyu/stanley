02cf7d2a31b3659bb0ffea6b9df44bbda39021db
Justin Yu <justin.v.yu@gmail.com>
2015-06-01 13:37:45 -0700

    First commit

---
+++ stanley/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  stanley
+//
+//  Created by Justin Yu on 6/1/15.
+//  Copyright (c) 2015 Justin Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ stanley/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  stanley
+//
+//  Created by Justin Yu on 6/1/15.
+//  Copyright (c) 2015 Justin Yu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
